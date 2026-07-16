a869ec7df320245b6799c44b9a24a7d038e0a8ea
1YOK2L <watpat.tanti@gmail.com>
2026-07-16 14:22:53 +0700

    16th commit

--- company/admin.py
+++ company/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import path
 
 # Register your models here.
 from .models import Company, Customer, District, ShippingAddress, Province, Subdistrict
@@ -24,9 +25,39 @@ class CustomerAdmin(admin.ModelAdmin):
 
 @admin.register(ShippingAddress)
 class ShippingAdmin(admin.ModelAdmin):
-    list_display = ("contact_name", "phone", "address", "subdistrict", "district", "province", "zip_code")
-    search_fields = ("contact_name", "phone")
-    actions = [report_customers]
+    list_display = (
+        "contact_name",
+        "phone",
+        "address",
+        "subdistrict",
+        "district",
+        "province",
+        "zip_code",
+    )
+
+    change_form_template = "admin/company/shippingaddress/change_form.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                "<uuid:object_id>/print/",
+                self.admin_site.admin_view(self.print_label),
+                name="company_shippingaddress_print",
+            ),
+        ]
+        return custom_urls + urls
+
+    def print_label(self, request, object_id):
+        shipping = get_object_or_404(ShippingAddress, pk=object_id)
+
+        return render(
+            request,
+            "shipping/shipping_address_list.html",
+            {
+                "shipping_addresses": [shipping],
+            },
+        )
 
 @admin.register(Province)
 class ProvinceAdmin(admin.ModelAdmin):
@@ -42,3 +73,13 @@ class DistrictAdmin(admin.ModelAdmin):
 class SubdistrictAdmin(admin.ModelAdmin):
     list_display = ("id", "name_th", "name_en")
     search_fields = ("name_th", "name_en")
+
+@admin.action(description="Print Shipping Labels")
+def print_labels(modeladmin, request, queryset):
+    return render(
+        request,
+        "shipping/shipping_address_list.html",
+        {
+            "shipping_addresses": queryset,
+        },
+    )
